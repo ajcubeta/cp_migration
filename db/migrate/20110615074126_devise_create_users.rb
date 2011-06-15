c5946bc13a94a131f7a5bd54511dd6f755453b25
ajcubeta <ajcubeta@gmail.com>
2011-06-16 01:00:08 +0800

    Migrate devise users table

--- db/migrate/20110615074126_devise_create_users.rb
+++ db/migrate/20110615074126_devise_create_users.rb
@@ -6,23 +6,192 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.rememberable
       t.trackable
 
-      # t.encryptable
-      # t.confirmable
-      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-      # t.token_authenticatable
+      t.string :display_name
+      t.string :email, :null => false
+      t.string :crypted_password, :null => false
+      # t.string :password_salt, :null => false
+      t.string :persistence_token, :null => false
+      t.string :perishable_token, :null => false
 
+      t.integer :login_count, :null => false, :default => 0
+      t.integer :failed_login_count, :null => false, :default => 0
+      t.datetime :current_login_at
+      t.datetime :last_login_at
+      t.string :current_login_ip
+      t.string :last_login_ip
+
+      # Person name
+      t.string :affix, :prefix
+      t.string :first_name, :middle_name, :last_name
+      t.string :legal_name
+
+      # Personal information
+      t.string :gender
+      t.integer :nationality_id, :birth_place_id, :permanent_resident_id
+      t.string :race, :religion, :marital_status
+      t.date :dob
+
+      t.datetime :profile_last_updated_at
+
+      # Remuneration package
+      # Default to per month, if want to view per annual, simply multiply it
+      t.decimal :expected_salary_min, :precision => 10, :scale => 2
+      t.decimal :expected_salary_max, :precision => 10, :scale => 2
+      t.decimal :current_salary, :precision => 10, :scale => 2
+
+      # Availability
+      t.string :availability
+
+      # Location
+      t.text :working_location
+      t.boolean :travel, :default => false
+      t.boolean :relocate, :default => false
+
+      # Shift
+      t.boolean :shift, :default => false
+
+      # tag
+      t.boolean :tagged, :default => false
+
+      # Counters
+      t.integer :legal_identifiers_count, :null => false, :default => 0
+      t.integer :addresses_count, :null => false, :default => 0
+      t.integer :websites_count, :null => false, :default => 0
+
+      t.integer :educations_count, :null => false, :default => 0
+      t.integer :experiences_count, :null => false, :default => 0
+      t.integer :competencies_count, :null => false, :default => 0
+      t.integer :professional_references_count, :null => false, :default => 0
+      t.integer :publications_count, :null => false, :default => 0
+      t.integer :languages_count, :null => false, :default => 0
+
+      t.integer :job_applications_count, :null => false, :default => 0
+
+      t.integer :job_watch_lists_count, :null => false, :default => 0
+      t.integer :job_block_lists_count, :null => false, :default => 0
+
+      t.integer :company_black_lists_count, :null => false, :default => 0
+
+      # AASM
+      t.string :status, :default => "inactive"
+      t.datetime :status_updated_at
+
+      # STI
+      # type=Employer|Consultant|Candidate
+      # NOTE: for object removal, only admin will directly remove object, while staff
+      # will set a flag for removal only
+      t.string :type
+      t.boolean :staff, :default => false
+      t.boolean :admin, :default => false
+
+      # Facebook
+      t.integer :facebook_uid, :limit => 8
+
+      # LinkedIn
+      t.boolean :linkedin_before, :default => false
+      t.string :linkedin_profile_url
+      t.string :linkedin_picture_url
+      t.string :access_token
+      t.string :access_secret
+
+      # FileMaker migration indicator
+      t.boolean :from_filemaker, :default => false
+
+      t.references :company, :department
+      t.string :designation
+
+      # Ratings - to calculate, we use rating_total/rating_count = average rating(rounded to 1 decimal)
+      t.integer :rating_total, :default => 0
+      t.integer :rating_count, :default => 0
+
+      # Timezone
+      t.string :timezone, :default => "Singapore"
+
+      # Currency code
+      t.string :currency_code, :limit => 3, :default => "SGD" # See http://en.wikipedia.org/wiki/ISO_4217
+
+      # Avatar
+      t.string :avatar_file_name
+      t.string :avatar_content_type
+      t.integer :avatar_file_size
+      t.datetime :avatar_updated_at
+
+      # Resume
+      t.string :resume_file_name
+      t.string :resume_content_type
+      t.integer :resume_file_size
+      t.datetime :resume_updated_at
+
+      # Phone numbers
+      t.string :home_number
+      t.string :mobile_number
+      t.string :work_number
+
+      # Highest education level
+      t.references :highest_education_level
+      t.text :skill_sets
+
+      # Receiving updates
+      t.boolean :job_alert, :default => true
+      t.boolean :notification, :default => true
+
+      # Sphinx search
+      t.boolean :delta, :default => false
 
       t.timestamps
     end
 
-    add_index :users, :email,                :unique => true
+    add_index :users, :email
+    add_index :users, :company_id
+    add_index :users, :facebook_uid
+    add_index :users, :nationality_id
+    add_index :users, :birth_place_id
+    add_index :users, :race
+    add_index :users, :religion
+    add_index :users, :department_id
+    add_index :users, :permanent_resident_id
+    add_index :users, :highest_education_level_id
+    add_index :users, :delta
+    add_index :users, :type
+    add_index :users, [:id, :type]
+    add_index :users, [:first_name, :last_name]
+    add_index :users, :first_name
+    add_index :users, :last_name
+    add_index :users, :status
+    add_index :users, :staff
+    add_index :users, :admin
+    add_index :users, :created_at
+    add_index :users, :updated_at
+    add_index :users, :status_updated_at
+    add_index :users, :tagged
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
-    # add_index :users, :unlock_token,         :unique => true
-    # add_index :users, :authentication_token, :unique => true
   end
 
   def self.down
+    remove_index :users, :status_updated_at
+    remove_index :users, :tagged
+    remove_index :users, :type
+    remove_index :users, :updated_at
+    remove_index :users, :created_at
+    remove_index :users, :admin
+    remove_index :users, :staff
+    remove_index :users, :status
+    remove_index :users, :last_name
+    remove_index :users, :first_name
+    remove_index :users, :column => [:first_name, :last_name]
+    remove_index :users, :column => [:id, :type]
+    remove_index :users, :delta
+    remove_index :users, :highest_education_level_id
+    remove_index :users, :permanent_resident_id
+    remove_index :users, :department_id
+    remove_index :users, :religion
+    remove_index :users, :race
+    remove_index :users, :birth_place_id
+    remove_index :users, :nationality_id
+    remove_index :users, :facebook_uid
+    remove_index :users, :company_id
+    remove_index :users, :email
+
     drop_table :users
   end
 end
@@ -61,4 +230,13 @@ end
 # 2. Add necessary attributes from original migration users.rb
 # Dump careerpacific_production.sql to PostreSQL cp_migration_development mode
 # 3. Create migration to update :crypted_password and other attributes on devise.
-# 4. Ask RH :)
+# 4. http://developer.postgresql.org/pgdocs/postgres/app-createuser.html
+
+# dropuser postgres
+# createuser postgres -p
+# role: root
+# rake db:create:all
+# rake db:migrate
+
+# rvm list
+# ruby-1.9.2-head
--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,125 @@
 ActiveRecord::Schema.define(:version => 20110615074126) do
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                                                                                :null => false
+    t.string   "encrypted_password",            :limit => 128,                                :default => "",          :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                                                               :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "display_name"
+    t.string   "crypted_password",                                                                                     :null => false
+    t.string   "persistence_token",                                                                                    :null => false
+    t.string   "perishable_token",                                                                                     :null => false
+    t.integer  "login_count",                                                                 :default => 0,           :null => false
+    t.integer  "failed_login_count",                                                          :default => 0,           :null => false
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.string   "affix"
+    t.string   "prefix"
+    t.string   "first_name"
+    t.string   "middle_name"
+    t.string   "last_name"
+    t.string   "legal_name"
+    t.string   "gender"
+    t.integer  "nationality_id"
+    t.integer  "birth_place_id"
+    t.integer  "permanent_resident_id"
+    t.string   "race"
+    t.string   "religion"
+    t.string   "marital_status"
+    t.date     "dob"
+    t.datetime "profile_last_updated_at"
+    t.decimal  "expected_salary_min",                          :precision => 10, :scale => 2
+    t.decimal  "expected_salary_max",                          :precision => 10, :scale => 2
+    t.decimal  "current_salary",                               :precision => 10, :scale => 2
+    t.string   "availability"
+    t.text     "working_location"
+    t.boolean  "travel",                                                                      :default => false
+    t.boolean  "relocate",                                                                    :default => false
+    t.boolean  "shift",                                                                       :default => false
+    t.boolean  "tagged",                                                                      :default => false
+    t.integer  "legal_identifiers_count",                                                     :default => 0,           :null => false
+    t.integer  "addresses_count",                                                             :default => 0,           :null => false
+    t.integer  "websites_count",                                                              :default => 0,           :null => false
+    t.integer  "educations_count",                                                            :default => 0,           :null => false
+    t.integer  "experiences_count",                                                           :default => 0,           :null => false
+    t.integer  "competencies_count",                                                          :default => 0,           :null => false
+    t.integer  "professional_references_count",                                               :default => 0,           :null => false
+    t.integer  "publications_count",                                                          :default => 0,           :null => false
+    t.integer  "languages_count",                                                             :default => 0,           :null => false
+    t.integer  "job_applications_count",                                                      :default => 0,           :null => false
+    t.integer  "job_watch_lists_count",                                                       :default => 0,           :null => false
+    t.integer  "job_block_lists_count",                                                       :default => 0,           :null => false
+    t.integer  "company_black_lists_count",                                                   :default => 0,           :null => false
+    t.string   "status",                                                                      :default => "inactive"
+    t.datetime "status_updated_at"
+    t.string   "type"
+    t.boolean  "staff",                                                                       :default => false
+    t.boolean  "admin",                                                                       :default => false
+    t.integer  "facebook_uid",                  :limit => 8
+    t.boolean  "linkedin_before",                                                             :default => false
+    t.string   "linkedin_profile_url"
+    t.string   "linkedin_picture_url"
+    t.string   "access_token"
+    t.string   "access_secret"
+    t.boolean  "from_filemaker",                                                              :default => false
+    t.integer  "company_id"
+    t.integer  "department_id"
+    t.string   "designation"
+    t.integer  "rating_total",                                                                :default => 0
+    t.integer  "rating_count",                                                                :default => 0
+    t.string   "timezone",                                                                    :default => "Singapore"
+    t.string   "currency_code",                 :limit => 3,                                  :default => "SGD"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string   "resume_file_name"
+    t.string   "resume_content_type"
+    t.integer  "resume_file_size"
+    t.datetime "resume_updated_at"
+    t.string   "home_number"
+    t.string   "mobile_number"
+    t.string   "work_number"
+    t.integer  "highest_education_level_id"
+    t.text     "skill_sets"
+    t.boolean  "job_alert",                                                                   :default => true
+    t.boolean  "notification",                                                                :default => true
+    t.boolean  "delta",                                                                       :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["admin"], :name => "index_users_on_admin"
+  add_index "users", ["birth_place_id"], :name => "index_users_on_birth_place_id"
+  add_index "users", ["company_id"], :name => "index_users_on_company_id"
+  add_index "users", ["created_at"], :name => "index_users_on_created_at"
+  add_index "users", ["delta"], :name => "index_users_on_delta"
+  add_index "users", ["department_id"], :name => "index_users_on_department_id"
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["facebook_uid"], :name => "index_users_on_facebook_uid"
+  add_index "users", ["first_name", "last_name"], :name => "index_users_on_first_name_and_last_name"
+  add_index "users", ["first_name"], :name => "index_users_on_first_name"
+  add_index "users", ["highest_education_level_id"], :name => "index_users_on_highest_education_level_id"
+  add_index "users", ["id", "type"], :name => "index_users_on_id_and_type"
+  add_index "users", ["last_name"], :name => "index_users_on_last_name"
+  add_index "users", ["nationality_id"], :name => "index_users_on_nationality_id"
+  add_index "users", ["permanent_resident_id"], :name => "index_users_on_permanent_resident_id"
+  add_index "users", ["race"], :name => "index_users_on_race"
+  add_index "users", ["religion"], :name => "index_users_on_religion"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["staff"], :name => "index_users_on_staff"
+  add_index "users", ["status"], :name => "index_users_on_status"
+  add_index "users", ["status_updated_at"], :name => "index_users_on_status_updated_at"
+  add_index "users", ["tagged"], :name => "index_users_on_tagged"
+  add_index "users", ["type"], :name => "index_users_on_type"
+  add_index "users", ["updated_at"], :name => "index_users_on_updated_at"
 
 end
